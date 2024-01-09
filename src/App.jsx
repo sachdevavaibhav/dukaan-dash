ae626297fc5012ac53475fc0e07e821c46a45711
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2024-01-09 07:06:47 +0530

    enhancement --check for mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import Sidebar from "./components/sidebar/Sidebar"
 
 function App() {
 
+  const isMobile = window.innerWidth <= 768;
+
+  if (isMobile) {
+    return (
+      <div className="flex w-full h-screen justify-center items-center">
+        <h1>Please switch to desktop view to use this application</h1>
+      </div>
+    )
+  }
+
   return (
     <div className="flex items-start">
       <Sidebar />
