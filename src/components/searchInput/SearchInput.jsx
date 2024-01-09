0f86df1985d9e15305963e2e93a968504da81fcd
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2024-01-09 07:00:09 +0530

    completed --payments page

--- src/components/searchInput/SearchInput.jsx
+++ src/components/searchInput/SearchInput.jsx
@@ -1,12 +1,13 @@
 import searchIcon from '../../assets/icons/search.svg';
 
-function SearchInput({placeholder}) {
+function SearchInput({placeholder, classes  ,...props}) {
+    classes = "input w-full ps-12 bg-[#F2F2F2] placeholder:text-[#808080] text-primary focus:border-none focus:outline-none" + " " + classes;
     return (
         <div className="w-full flex justify-center text-inter relative">
             <div className="absolute left-5 top-1/3 w-5">
                 <img src={searchIcon} alt="Search Icon" className='w-full' />
             </div>
-            <input type="text" placeholder={placeholder} className="input w-full ps-12 bg-[#F2F2F2] placeholder:text-[#808080] text-primary focus:border-none focus:outline-none" />
+            <input type="text" placeholder={placeholder} className={classes} {...props} />
         </div>
     )
 }
--- src/pages/payments/Payments.jsx
+++ src/pages/payments/Payments.jsx
@@ -2,7 +2,11 @@ import Header from "../../components/header/Header"
 import StatCard from "../../components/statCard/StatCard";
 import Table from "./components/Table";
 import Pagination from "./components/Pagination.jsx";
+import SearchInput from "../../components/searchInput/SearchInput.jsx";
 import {data} from "./data.js"
+import sortIcon from "../../assets/icons/Sort.svg";
+import downloadIcon from "../../assets/icons/download.svg";
+import downArrowIcon from "../../assets/icons/arrow.svg";
 
 function Payments() {
     return (
@@ -11,7 +15,10 @@ function Payments() {
             <section className="px-10 pt-10">
                 <div className="flex justify-between mb-7">
                     <h2 className="text-xl font-inter font-medium text-primary">Overview</h2>
-                    <h2 className="text-xl font-inter font-medium text-primary">Overview</h2>
+                    <button className="btn no-animation bg-transparent text-secondary font-medium text-sm border border-[#D9D9D9] hover:border-[#D9D9D9] hover:bg-transparent">
+                        Last Month
+                        <img src={downArrowIcon} />
+                    </button>
                 </div>
                 <div className="flex justify-between gap-8 mb-7">
                     <StatCard title="Online orders" value="231" />
@@ -21,6 +28,20 @@ function Payments() {
                     <p className="text-xl font-inter font-medium text-primary">Transaction | This Month</p>
                 </div>
                 <div className="mb-7 bg-white rounded-lg p-5">
+                    <div className="w-full flex justify-between mb-3">
+                        <div>
+                            <SearchInput placeholder="Search by order id ..." classes="bg-transparent border border-[#D9D9D9] focus:border-[#D9D9D9] placeholder:text-[#999] font-normal" />
+                        </div>
+                        <div className="flex gap-4">
+                            <button className="btn no-animation bg-transparent text-secondary font-medium text-sm border border-[#D9D9D9] hover:border-[#D9D9D9] hover:bg-transparent">
+                                Sort
+                                <img src={sortIcon} alt="previous" />
+                            </button>
+                            <button className="btn no-animation bg-transparent text-secondary font-medium text-sm border border-[#D9D9D9] hover:border-[#D9D9D9] hover:bg-transparent">
+                                <img src={downloadIcon} alt="previous" />
+                            </button>
+                        </div>
+                    </div>
                     <Table data={data} />
                     <div className="w-full flex justify-center mt-5">
                         <Pagination />
