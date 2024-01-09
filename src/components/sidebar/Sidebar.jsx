946f86b04c3101ef014bf954c645dbdf8d8c2450
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2024-01-09 20:55:30 +0530

    fix --fixed responsive bugs

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,20 +5,19 @@ import hamburgerIcon from "../../assets/icons/hamburger.svg";
 function Sidebar() {
     return (
         <>
-            <div className="drawer absolute lg:hidden top-[2%] left-3">
+            <div className="drawer absolute lg:hidden top-4 left-3 overflow-hidden">
                 <input id="my-drawer" type="checkbox" className="drawer-toggle" />
                     <div className="drawer-content">
-                        {/* Page content here */}
-                        <label htmlFor="my-drawer" className="drawer-button">
-                            <img src={hamburgerIcon} alt="" />
+                        <label htmlFor="my-drawer" className="drawer-button h-full">
+                            <img src={hamburgerIcon} alt="Navigation Menu" />
                         </label>
                     </div>
-                <div className="drawer-side z-50">
-                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <div className="fixed left-0 flex flex-col bg-navy text-white h-screen w-full max-w-[13rem] xl:max-w-[14rem]">
-                        <SidebarContent/>
+                    <div className="drawer-side z-50 h-full">
+                        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                        <div className="fixed left-0 flex flex-col bg-navy text-white h-full w-full max-w-[13rem] xl:max-w-[14rem] overflow-hidden">
+                            <SidebarContent/>
+                        </div>
                     </div>
-                </div>
             </div>
             <div className="fixed hidden lg:flex left-0 flex-col bg-navy text-white h-screen w-full max-w-[13rem] xl:max-w-[14rem]">
                 <SidebarContent />
--- src/pages/payments/Payments.jsx
+++ src/pages/payments/Payments.jsx
@@ -25,7 +25,7 @@ function Payments() {
                     <StatCard title="Amount received" value="₹23,92,312.19" />
                 </div>
                 <div className="mb-7">
-                    <p className="text-xl font-inter font-medium text-primary">Transaction | This Month</p>
+                    <p className="text-xl font-inter font-medium text-primary text-center md:text-left">Transaction | This Month</p>
                 </div>
                 <div className="mb-7 bg-white rounded-lg p-5">
                     <div className="w-full flex flex-col md:flex-row justify-between mb-3">
